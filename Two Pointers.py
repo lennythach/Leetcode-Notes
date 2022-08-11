fe3deb5b57ed470eece206a82ad6268a5462a902
Lenny Thach <lennythach1@gmail.com>
2022-08-11 14:10:11 -0700

    updating to py files

---
+++ Two Pointers.py
@@ -0,0 +1,22 @@
+'''
+Consider using a two pointer when you are jumping over multiple iterations.
+Example Problem: 
+
+Leetcode problem:
+    Encode and Decode String
+
+'''
+
+'''
+If you ever need to return 3 values from and array it most likely require 0(n**2) approach.
+Best way to do it is: 
+    - by using a for loop for the 1st value
+    - use a two pointer to get the the other 2 value
+
+Leetcode problem:
+    3sum
+    
+BIG TAKE AWAY:
+    DONT BE AFRAID TO GO FOR A NLOGN SOLUTION!!!!
+    use the sorting as long as you can get the answer first.
+'''
---
+++ hashing.py
@@ -0,0 +1,8 @@
+
+'''
+If consecutive sequence encounter consider using a set for faster look up time.
+Also keep track of the current sequence and maximum sequence.
+to make look up time shorter look for the beginning of the sequence by adding a condition to check if the current iteration is the beginning of the sequence. Example: if num-1 not in set:
+
+'''
+
